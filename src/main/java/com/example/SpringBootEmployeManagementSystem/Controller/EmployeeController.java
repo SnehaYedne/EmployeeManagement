209a872b9f8207072eafd805f06e6f4bd32689db
SnehaYedne <121806899+SnehaYedne@users.noreply.github.com>
2024-06-28 19:27:40 +0530

    Update EmployeeController.java

--- src/main/java/com/example/SpringBootEmployeManagementSystem/Controller/EmployeeController.java
+++ src/main/java/com/example/SpringBootEmployeManagementSystem/Controller/EmployeeController.java
@@ -1,2 +1,57 @@
-package com.example.SpringBootEmployeManagementSystem.Controller;public class EmployeeController {
+
+package com.example.SpringBootEmployeManagementSystem.Controller;
+
+import com.example.SpringBootEmployeManagementSystem.Entity.Employee;
+import com.example.SpringBootEmployeManagementSystem.Exception.EmployeeNotFoundException;
+import com.example.SpringBootEmployeManagementSystem.Repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+   @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/Employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping("/Employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
+
+        employee.setName(employeeDetails.getName());
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
+
+        employeeRepository.delete(employee);
+        return ResponseEntity.noContent().build();
+    }
+
+}
 }
