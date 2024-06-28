103432c450d7ee38001631402f26174a37f24512
SnehaYedne <121806899+SnehaYedne@users.noreply.github.com>
2024-06-28 19:26:22 +0530

    Update EmployeeNotFoundException.java

--- src/main/java/com/example/SpringBootEmployeManagementSystem/Exception/EmployeeNotFoundException.java
+++ src/main/java/com/example/SpringBootEmployeManagementSystem/Exception/EmployeeNotFoundException.java
@@ -1,2 +1,14 @@
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND)
+
 package com.example.SpringBootEmployeManagementSystem.Exception;public class EmployeeNotFoundException {
+   private static final long serialVersionID=5088656766912L;
+    public  EmployeeNotFoundException(String message){
+        super(message);
+    }
 }
+
+   
